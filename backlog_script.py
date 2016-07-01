a3132fd0b00842cb81c12a5bcb4d19fc84542987
priyankamandikal <priyanka.mp2808@gmail.com>
2016-07-02 02:48:19 +0530

    Creates question files in records directory

--- backlog_script.py
+++ backlog_script.py
@@ -1,21 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
 import traceback
+from os import path, listdir
 
 url = 'https://en.wikipedia.org/wiki/Category:Wikipedia_articles_in_need_of_updating_from_May_2016'
+categ = path.basename(url)
 
-try:
-	r = requests.get(url)
-	bs = BeautifulSoup(r.text)
-	for catgroup in bs.find_all('div', 'mw-category-group'):
-		for entry in catgroup.find_all('li'):
-			a = entry.find('a')
-			link = 'https://en.wikipedia.org' + a.get('href')
-			title = a.get('title')
-			print title
-			print link
-			print
+recdir = 'records' + path.sep
 
-except:
-	print "Error while parsing backlogs"
-	print traceback.format_exc()
+def nextrecord():
+	try:
+		records = listdir(recdir)
+		record = 1+int(max(records)[:9])
+		### todo: check for improperly named files
+		return format(record, '09')
+	except:
+		return format(1, '09')
+
+if __name__ == '__main__':
+
+	try:
+		r = requests.get(url)
+		bs = BeautifulSoup(r.text)
+		for catgroup in bs.find_all('div', 'mw-category-group'):
+			for entry in catgroup.find_all('li'):
+				a = entry.find('a')
+				link = 'https://en.wikipedia.org' + a.get('href')
+				title = a.get('title').encode('utf8')
+				fn = recdir + nextrecord() + 'q'
+				print fn
+				if path.exists(fn):
+					print('A billion questions reached! Answer!')
+					exit()
+				f = open(fn, 'w')
+				f.write(categ + '\n' + title + '\n' + url +'\n')	# in file, print <backlog category>, <title>, <url>
+				f.close()
+
+	except:
+		print "Error while parsing backlogs"
+		print traceback.format_exc()
